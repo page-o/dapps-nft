039e7ff619e9daf1f80430cd166679f5c7d73e36
IrieRyuga <ryuga.irie@sun-asterisk.com>
2022-06-15 14:13:06 +0900

    delete: delte unnecessary code

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,7 @@
-const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider")
 const path = require("path");
-const { INFURA_API_URL, MNEMONIC } = process.env;
+const { INFURA_API_URL, ALCHEMY_API_URL, MNEMONIC, INDEX_OF_YOUR_ADDRESS } = process.env;
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
@@ -11,9 +12,18 @@ module.exports = {
       network_id: "5777"
     },
     ropsten: {
-      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_URL),
+      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_URL, INDEX_OF_YOUR_ADDRESS),
       network_id: 3,
       gas: 5500000
+    },
+    mumbai: {
+      provider: () => new HDWalletProvider(MNEMONIC, ALCHEMY_API_URL, INDEX_OF_YOUR_ADDRESS),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      gas: 6000000,
+      gasPrice: 10000000000,
     }
   },
   compilers: {
